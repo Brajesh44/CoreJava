da2a8f8e5fb66a5cfbf075283072253cdb847d72
Brajesh <Brajesh@brajesh>
2021-03-08 21:31:32 +0530

    Parent and Child ThreadGroup name

---
+++ CoreJavaMuiltithreading/src/thread/group/name/FindingNameOfThreadGroup.java
@@ -0,0 +1,11 @@
+package thread.group.name;
+
+public class FindingNameOfThreadGroup {
+
+	public static void main(String[] args) {
+		System.out.println(Thread.currentThread().getThreadGroup().getName());
+		System.out.println(Thread.currentThread().getThreadGroup().getParent().getName());
+
+	}
+
+}
