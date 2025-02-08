4c54d09b8dde10d3ff90a832a4343c976aed0f28
Brajesh44 <brajesh.gautam2@gmail.com>
2025-02-08 13:20:23 +0530

    Change massage

--- CoreJavaMuiltithreading/src/child/main/thread/excution/TestNamingThread.java
+++ CoreJavaMuiltithreading/src/child/main/thread/excution/TestNamingThread.java
@@ -5,7 +5,7 @@ public class TestNamingThread {
 	public static void main(String[] args) {
 		Mythread mt= new Mythread();
 		mt.start();
-		System.out.println("Main method excuted by thread=="+Thread.currentThread().getName());
+		System.out.println("Main method executed by thread=="+Thread.currentThread().getName());
 	}
 
 }
